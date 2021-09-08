eaa0ad0c0579609fd49e345ae0251cd2a1ecf951
frontend225 <tfahmed@yahoo.com>
2021-09-07 20:44:23 -0700

    commit

---
+++ Develop/index.js
@@ -0,0 +1,35 @@
+<div class="container">
+      <input placeholder="Input text" name="name"/>
+      <p id="values"></p>
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+      moment().format(); // Date and Timeblock
+    </div>
